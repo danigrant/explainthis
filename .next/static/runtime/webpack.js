834e01c6eb49ae394eeee714ceec22cadfc8084a
dani <mayor@dani.town>
2019-11-11 22:52:10 -0500

    handle success with fireworks

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "22bf61db577e332257a3";
+/******/ 	var hotCurrentHash = "906147adfe11597f26a1";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
